5a3052b79fb67b498718efc19e281266c9b2e3c6
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-12-08 17:20:49 +0100

    Notification component

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,22 +1,42 @@
 import css from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
+import { Component } from 'react';
 
-export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-  return (
-    <div className={css.feedback}>
-      {options.map(option => (
-        <button
-          key={option}
-          type="button"
-          name={option}
-          onClick={onLeaveFeedback}
-        >
-          {option}
-        </button>
-      ))}
-    </div>
-  );
-};
+export class FeedbackOptions extends Component {
+  render() {
+    return (
+      <div className={css.feedback}>
+        {this.props.options.map(option => (
+          <button
+            key={option}
+            type="button"
+            name={option}
+            onClick={this.props.onLeaveFeedback}
+          >
+            {option}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
+
+// export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+//   return (
+//     <div className={css.feedback}>
+//       {options.map(option => (
+//         <button
+//           key={option}
+//           type="button"
+//           name={option}
+//           onClick={onLeaveFeedback}
+//         >
+//           {option}
+//         </button>
+//       ))}
+//     </div>
+//   );
+// };
 
 FeedbackOptions.propTypes = {
   options: PropTypes.arrayOf(PropTypes.string),
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Section } from 'components/Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
+import { Notification } from 'components/Notification/Notification';
 
 import './index.css';
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Section title="Please leave feedback">
       <FeedbackOptions options={['good', 'neutral', 'bad']} />
       <Statistics />
+      <Notification message="No feedback given" />
     </Section>
   </React.StrictMode>
 );
