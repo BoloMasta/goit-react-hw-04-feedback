05b84d782bf76df658399846de832c90f069663e
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-12-15 13:01:52 +0100

    update propTypes for Positive feedback

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -31,4 +31,5 @@ Statistics.propTypes = {
   neutral: PropTypes.number,
   bad: PropTypes.number,
   total: PropTypes.number,
+  positivePercentage: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
 };
