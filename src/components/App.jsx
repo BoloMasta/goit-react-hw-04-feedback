b67dc0d77f8c9df191dc2b8b31fde3585d3137b9
Bolek <103572991+BoloMasta@users.noreply.github.com>
2022-12-22 14:22:58 +0100

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,7 +29,7 @@ export const App = () => {
 
   return (
     <>
-      <Section title="Please leave feedback TEST 45">
+      <Section title="Please leave feedback TEST 456">
         <FeedbackOptions
           options={Object.keys(state)}
           onLeaveFeedback={onLeaveFeedback}
